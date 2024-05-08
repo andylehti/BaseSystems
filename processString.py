ace7772da4c7313984f15cb6057e0f32e1671260
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2024-05-08 00:19:44 -0500

    Update processString.py
    updated to personal preferred style

--- processString.py
+++ processString.py
@@ -1,11 +1,11 @@
 # additional functions to process strings of numbers separated by numbers all at once, i.e. convertBase('2154 53656 63 7434 2456', 32) -> convertDecimal('23a 1kco 1v 78a 2co', 32) -> '2154 53656 63 7434 2456'
 
-def convertBase(input_str, base):
-    numbers_str_list = input_str.split()
-    binary_numbers = [toBase(int(number), base) for number in numbers_str_list]
-    return ' '.join(binary_numbers)
+def convertBase(a, b):
+    c = a.split()
+    d = [toBase(int(e), b) for e in c]
+    return ' '.join(d)
 
-def convertDecimal(input_str, base):
-    numbers_str_list = input_str.split()
-    binary_numbers = [fromBase(number, base) for number in numbers_str_list]
-    return ' '.join(map(str, binary_numbers))
+def convertDecimal(a, b):
+    c = a.split()
+    d = [fromBase(e, b) for e in c]
+    return ' '.join(map(str, d))
