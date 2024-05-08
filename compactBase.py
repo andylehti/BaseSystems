6ed1dcb879a111d179047db80751d00cc24183a0
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2024-05-07 23:32:33 -0500

    Create compactBase.py

---
+++ compactBase.py
@@ -0,0 +1,16 @@
+import string
+import numpy as np
+
+def toBase(d, b):
+    c, n, r = string.printable[:90], 1, d
+    while r >= b ** n: r -= b ** n; n += 1
+    s = ""
+    while r > 0: s = c[r % b] + s; r //= b
+    return s.zfill(n)
+
+def toDecimal(c, b):
+    chars = string.printable[:90]
+    v, l = 0, len(c)
+    for i, ch in enumerate(reversed(c)):
+        v += chars.index(ch) * (b ** i)
+    return v + sum(b ** i for i in range(1, l))
