7fbc4c69ae7ce2abed5cb44e187fab8b04fb8ce0
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2024-05-08 00:37:09 -0500

    Delete systemSafePrintable.py

--- systemSafePrintable.py
+++
@@ -1,2 +0,0 @@
-# to enhance compatibility with sensitive systems, replace 'c = printable[:90]' with the following snippet, which appends special symbols. Limit the use of these symbols to within base82.
-c = ''.join([x for x in string.printable[:90] if x not in '/\\`"\',_']) + '/\\`"\',_'
