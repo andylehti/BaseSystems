fd765c926e1c17697e1a8006a9fc70c6c92d8524
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2024-05-08 00:06:38 -0500

    Update compactBase.py

--- compactBase.py
+++ compactBase.py
@@ -1,15 +1,17 @@
 import string
 import numpy as np
 
+def getBase(c): c = ''.join([x for x in string.printable[:c] if x not in '/\\`"\',_']) + '/\\`"\',_'; return c
+
 def toBase(d, b):
-    c, n, r = string.printable[:90], 1, d
+    c, n, r = getBase(b), 1, d
     while r >= b ** n: r -= b ** n; n += 1
     s = ""
     while r > 0: s = c[r % b] + s; r //= b
     return s.zfill(n)
 
-def fromBase(c, b): # converts any base that may contain leading zeroes to decimal to preserve the data
-    chars = string.printable[:90]
+def fromBase(c, b): # converts bases with leading zeroes to decimal to maintain data integrity.
+    chars = getBase(b)
     v, l = 0, len(c)
     for i, ch in enumerate(reversed(c)):
         v += chars.index(ch) * (b ** i)
