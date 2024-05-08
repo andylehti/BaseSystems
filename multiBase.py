0fd243cd0d54113ff60e1c55718c0328fc6a4deb
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2024-05-08 00:27:59 -0500

    Create multiBase.py

---
+++ multiBase.py
@@ -0,0 +1,21 @@
+def toMultiBase(n):
+    r, b = [], 1
+    while n > 0:
+        r.append((n % b) + 1)
+        n //= b
+        b += 1
+    return ' '.join(map(str, r))
+
+def fromMultiBase(vs):
+    vs, n, p, b = list(map(lambda x: x-1, map(int, vs.split()))), 0, 1, 1
+    for v in vs:
+        n += v * p
+        p *= b
+        b += 1
+    return n
+
+# for i in range(1, 10000):
+#    a = toMultiBase(i)
+#    b = fromMultiBase(a)
+#    print(i, a, b, b == i)
+# test using the above code snippet
