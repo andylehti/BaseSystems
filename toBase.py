0141f311c4bba8ac67edd12f30ed90324c3115b4
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2024-05-08 00:06:22 -0500

    Update toBase.py
    added safe base characters as getBase

--- toBase.py
+++ toBase.py
@@ -1,9 +1,11 @@
 import string
 
+def getBase(c): c = ''.join([x for x in string.printable[:c] if x not in '/\\`"\',_']) + '/\\`"\',_'; return c
+
 def toBase(n, b):
-    c = string.printable[:90]
+    c = getBase(b)
     return '' if n == 0 else toBase(n // b, b) + c[n % b]
 
 def fromBase(s, b):
-    c = string.printable[:90]
+    c = getBase(b)
     return sum(c.index(d) * b**i for i, d in enumerate(reversed(s)))
