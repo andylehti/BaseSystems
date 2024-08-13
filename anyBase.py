73d85a296b41a127513db4acda25121334cbd5f6
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2024-08-13 01:02:56 -0500

    Update anyBase.py
    renamed the function to match the original

--- anyBase.py
+++ anyBase.py
@@ -1,4 +1,4 @@
 import mpmath as mp
 
 def anyBase(n, b=12): mp.mp.dps = len(str(n)) * 2; return [n // b ** i % b for i in range(int(mp.log(n, b)), -1, -1)]
-def anyInverse(cn, b=12): mp.mp.dps = len(cn) * 2; return sum(d * b ** i for i, d in enumerate(reversed(cn)))
+def fromAnyBase(cn, b=12): mp.mp.dps = len(cn) * 2; return sum(d * b ** i for i, d in enumerate(reversed(cn)))
