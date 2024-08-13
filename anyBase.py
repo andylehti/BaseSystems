3386c0da6a0bdbbf0d150a84c3b1b7538002ba23
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2024-08-13 01:01:59 -0500

    Update anyBase.py
    changed anyBase to work with arbitrary precision. Some very large numbers in large bases were not returning the correct value. They inversed but was not in proper order.

--- anyBase.py
+++ anyBase.py
@@ -1,7 +1,4 @@
-import math
+import mpmath as mp
 
-def anyBase(n, b=1496):
-    return '0' if n == 0 else ' '.join(str(n // b ** i % b) for i in range(int(math.log(n, b)), -1, -1))
-
-def fromAnyBase(n, b=1496):
-    return sum(int(c) * b ** i for i, c in enumerate(reversed(n.split(' '))))
+def anyBase(n, b=12): mp.mp.dps = len(str(n)) * 2; return [n // b ** i % b for i in range(int(mp.log(n, b)), -1, -1)]
+def anyInverse(cn, b=12): mp.mp.dps = len(cn) * 2; return sum(d * b ** i for i, d in enumerate(reversed(cn)))
